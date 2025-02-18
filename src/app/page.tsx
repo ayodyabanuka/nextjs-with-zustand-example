fb8ea3cd67c806eddada187650297eb13c2290a7
ayodyabanuka <ayodyabanukafernando@gmail.com>
2025-02-18 16:14:16 +0530

    Full code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,50 @@
-import Image from "next/image";
+'use client';
+
+import { useCounterStore } from '@/store/counter';
+import { MdOutlineContentCopy } from 'react-icons/md';
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const count = useCounterStore((state) => state.count); //get count state from store
+  const increment = useCounterStore((state) => state.increment);
+  const decrement = useCounterStore((state) => state.decrement);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+  return (
+    <div className='flex justify-center items-center h-screen w-full flex-col max-w-screen-sm mx-auto gap-5 text-white/60 px-4  bg-[#070707] bg-dot-white/[0.1]'>
+      <div className='text-center '>
+        <div className='text-xl font-thin'>This project for learn</div>
+        <div className='text-5xl font-black text-yellow-400'>Zustand</div>
+        <div className='text-2xl font-bold'>State Management Tool</div>
+      </div>
+      <div className='flex flex-col justify-center items-center gap-2 p-5'>
+        Install zustand :{' '}
+        <button
+          className='hover:bg-zinc-700 bg-zinc-800 rounded-full cursor-pointer'
+          onClick={() => navigator.clipboard.writeText('npm install zustand')}
+        >
+          <code className='px-5 py-3  rounded-full flex items-center gap-4'>
+            npm install zustand <MdOutlineContentCopy />
+          </code>
+        </button>
+      </div>
+      <div className='flex flex-col gap-5  border-white/5 w-full mx-4 border-[0.1px] items-center justify-center rounded-lg p-10 bg-zinc-900/30 backdrop-blur-xl'>
+        <div>In here i have created counter</div>
+
+        <div className='text-4xl'>Counter: {count}</div>
+        <div className='flex gap-5'>
+          <button
+            className='px-3 py-2 bg-zinc-900 rounded-xl hover:bg-zinc-800'
+            onClick={increment}
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            Increment +
+          </button>
+          <button
+            className='px-3 py-2 bg-zinc-900 rounded-xl hover:bg-zinc-800'
+            onClick={decrement}
           >
-            Read our docs
-          </a>
+            Decrement -
+          </button>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,41 @@
-import type { Config } from "tailwindcss";
-
+import type { Config } from 'tailwindcss';
+import { PluginAPI } from 'tailwindcss/types/config';
+import svgToDataUri from 'mini-svg-data-uri';
+const {
+  default: flattenColorPalette,
+} = require('tailwindcss/lib/util/flattenColorPalette');
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+    extend: {},
   },
-  plugins: [],
+  plugins: [
+    function ({ matchUtilities, theme }: PluginAPI) {
+      matchUtilities(
+        {
+          'bg-dot': (value: string) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="1.6257413380501518"></circle></svg>`
+            )}")`,
+          }),
+        },
+        { values: flattenColorPalette(theme('backgroundColor')), type: 'color' }
+      );
+    },
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    function addVariablesForColors({ addBase, theme }: any) {
+      const allColors = flattenColorPalette(theme('colors'));
+      const newVars = Object.fromEntries(
+        Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+      );
+
+      addBase({
+        ':root': newVars,
+      });
+    },
+  ],
 } satisfies Config;
